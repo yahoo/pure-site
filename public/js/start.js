1d0c82a9b50ae33ebe9097baef7e1bac1b22a188
Tilo Mitra <tilomitra@gmail.com>
2014-01-28 17:02:09 -0500

    instantiate and demo Y.GridModel use case

--- public/js/start.js
+++ public/js/start.js
@@ -1,18 +1,12 @@
-YUI().use('css-mediaquery', 'rework', 'rework-pure-grids', function (Y) {
+YUI({
+    modules: {
+        'grid-model': '/js/models/grid-model.js'
+    }
+}).use('grid-model', function (Y) {
+
     'use strict';
 
-    var exported   = Y.Env._exported,
-        rework     = Y.config.global.rework,
-        pureGrids  = exported['rework-pure-grids'],
-        mediaQuery = exported['css-mediaquery'];
-
-    var css = rework('').use(pureGrids.units({
-        mediaQueries: {
-            med: 'screen and (min-width: 48em)',
-            lrg: 'screen and (min-width: 75em)'
-        }
-    })).toString();
-
-    console.log(mediaQuery.parse('screen and (min-width: 48em)'));
-    console.log(css);
+    var gridModel = new Y.GridModel(app.start.query);
+    console.log(gridModel.generate());
+    console.log(gridModel.toJSON());
 });
